b117e7657bd26ed38f0a2632b94845b7a31a262f
pgndvd <pgndvd@gmail.com>
2014-11-30 16:47:24 -0600

    granularity

--- Granularity.py
+++ Granularity.py
@@ -1,4 +1,4 @@
-import pycurl
+'''import pycurl
 
 from StringIO import StringIO
 
@@ -12,3 +12,65 @@ c.perform()
 c.close()
 result = b.getvalue()
 print(result)
+'''
+
+import json
+import pycurl
+import re
+
+from StringIO import StringIO
+
+
+sentenceArray = ["hello i'm asd beautfil", "asd no ", "asd hey"]
+
+
+def Granularity(sentenceArray):
+    bad = 0
+    good = 0
+    for sentence in sentenceArray:
+        buf = StringIO()
+        c = pycurl.Curl()
+        c.setopt(c.URL, 'http://text-processing.com/api/sentiment/')
+        c.setopt(c.WRITEFUNCTION, buf.write)
+        c.setopt(c.HTTPHEADER, ['Accept-Charset: UTF-8'])
+        c.setopt(c.POSTFIELDS, "text=" + sentence)
+        c.perform()
+        c.close()
+        data = buf.getvalue()
+        info = json.dumps(data)
+        json_object = json.loads(json.loads(info))
+        print
+        json_object['label']
+        if str(json_object['label']) == "neg":
+            bad = bad + 1
+        if str(json_object['label']) == "pos":
+            good = good + 1
+    print
+    good
+    print
+    bad
+    if good >= bad:
+        print
+        "good word"
+    else:
+        print
+        "bad word"
+
+
+list = []
+
+
+def getWords(sentence):
+    return re.compile('[A-Za-z]+').findall(sentence)
+
+
+def wordGran(sentenceArray):
+    list = []
+    good = 0
+    bad = 0
+    for sentence in sentenceArray:
+        list = list + getWords(sentence)
+    Granularity(list)
+
+
+wordGran(sentenceArray)
